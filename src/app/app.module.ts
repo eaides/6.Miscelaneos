3dbafb510c3f7afe952b7467578da7317ea01a03
ernesto <ernesto@signature-it.com>
2018-01-23 12:36:45 +0200

    directivas personalizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
 import { ClassesComponent } from './components/classes/classes.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ClassesComponent } from './components/classes/classes.component';
     AppComponent,
     NgStyleComponent,
     CssComponent,
-    ClassesComponent
+    ClassesComponent,
+    ResaltadoDirective
   ],
   imports: [
     BrowserModule
