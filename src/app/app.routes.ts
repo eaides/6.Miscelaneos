10d893705098e0387e22f7cca5eb573238de5e6f
ernesto <ernesto@signature-it.com>
2018-01-24 15:29:23 +0200

    routes father and children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
+
 const APP_ROUTES: Routes = [
-  {path: 'home', pathMatch: 'full', component: HomeComponent },
-  {path: 'usuario', pathMatch: 'full', component: UsuarioComponent },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'usuario/:id',
+    component: UsuarioComponent,
+    children: USUARIO_ROUTES
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
