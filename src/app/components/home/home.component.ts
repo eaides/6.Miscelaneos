5f1bfcb1be2c1274905569c4838c9706f51865bd
ernesto <ernesto@signature-it.com>
2018-01-25 11:28:59 +0200

    cyle of life hooks

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,29 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit,
+        AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
 
     <app-ng-style></app-ng-style>
+    <br>
 
     <app-css></app-css>
+    <br>
 
     <app-classes></app-classes>
+    <br><br>
 
     <p [appResaltado]="'red'">
-    Hola Mundo
+      Hola Mundo
     </p>
+    <br>
 
     <app-ng-swith></app-ng-swith>
 
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log('inside constructor');
+  }
 
   ngOnInit() {
+    console.log('inside ngOnInit');
   }
+
+  ngOnChanges(): void {
+    console.log('inside ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('inside ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('inside ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('inside ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('inside ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('inside ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('inside ngOnDestroy');
+  }
+
 
 }
